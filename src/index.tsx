c738e1b1906c56069e8578816fc34d57efd036d7
Jana Kunft <jana.kunft@and.digital>
2024-10-13 16:37:17 +0100

    with working delete button, before changing food data structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-import { GlobalStyles } from '@contentful/f36-components';
-import { SDKProvider } from '@contentful/react-apps-toolkit';
+import { GlobalStyles } from "@contentful/f36-components";
+import { SDKProvider } from "@contentful/react-apps-toolkit";
 
-import App from './App';
-import LocalhostWarning from './components/LocalhostWarning';
+import App from "./App";
+import LocalhostWarning from "./components/LocalhostWarning";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && window.self === window.top) {
   // You can remove this if block before deploying your app
--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,39 +1,52 @@
 import { FieldExtensionSDK } from "@contentful/app-sdk";
-import { Paragraph, TextInput } from "@contentful/f36-components";
+import { Box, Flex, TextInput } from "@contentful/f36-components";
+import { DeleteIcon } from "@contentful/f36-icons";
 import { useSDK } from "@contentful/react-apps-toolkit";
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const APP_ID = "1bd0d702";
+const APP_KEY = "32b6ab458c23061345da3a4540c17a11";
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
-  const initialRows: Ingredient[] = [
-    {
-      id: "khifehvi01",
-      name: "banana",
-    },
-    {
-      id: "kfehvi01",
-      name: "egg",
-    },
-  ];
+  const fieldValue = sdk.field.getValue();
+  const initialRows: Ingredient[] = fieldValue || [];
 
   const [rows, setRows] = useState<Ingredient[]>(initialRows);
   const [value, setValue] = useState("");
 
-  const processEntry = () => {
-    console.log("Processing entry: " + value);
-    setRows(prevRows => [
-      ...prevRows,
+  const getFoodData = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/api/nutrition-data?app_id=${APP_ID}&app_key=${APP_KEY}&nutrition-type=cooking&ingr=${value}`
+    );
+    return await response.json();
+  };
+
+  const processEntry = async () => {
+    const foodData = await getFoodData();
+    console.log(foodData);
+    const newRows = [
+      ...rows,
       {
-        id: value,
-        name: value,
+        id: uuidv4(),
+        text: value,
+        parsed: foodData.ingredients[0]?.parsed,
       },
-    ]);
-    console.log(rows);
+    ];
+    setRows(newRows);
+    sdk.field.setValue(newRows);
+
     setValue("");
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") processEntry();
+  };
+
+  const onDeleteButtonClicked = (passedRow: Ingredient) => {
+    const updatedRows = rows.filter(row => row !== passedRow);
+    setRows(updatedRows);
   };
 
   useEffect(() => {
@@ -42,17 +55,36 @@ const Field = () => {
 
   return (
     <>
-      {rows.map(row => (
-        <Paragraph key={row.id}>{row.name}</Paragraph>
-      ))}
+      <Flex flexDirection="column">
+        {rows.map(row => {
+          const parsed = row.parsed;
+          const foodData = parsed ? parsed[0] : undefined;
+          // console.log(JSON.stringify(row));
+          // const {quantity} = foodData
+          return (
+            <Box key={row.id} padding="spacingXs" marginRight="spacingM">
+              <Flex justifyContent="space-between">
+                <span>
+                  <strong>
+                    {foodData?.quantity} {foodData?.measure}
+                  </strong>{" "}
+                  {foodData?.foodMatch}
+                </span>
+                <DeleteIcon variant="primary" onClick={() => onDeleteButtonClicked(row)} />
+              </Flex>
+            </Box>
+          );
+        })}
+      </Flex>
       <TextInput
         type="email"
         name="email"
         placeholder="Add an ingredient"
         onKeyDown={onKeyDown}
         onChange={e => setValue(e.target.value)}
-        onBlur={processEntry}
+        // onBlur={processEntry}
         value={value}
+        autoComplete="false"
       />
     </>
   );
